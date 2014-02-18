b3ed064dcff4aa0c05ef823697a1e94e3636345f
James Stackhouse <jaffajames@gmail.com>
2014-02-18 15:48:34 +1100

    typo fixes and hj section

--- js/app.js
+++ js/app.js
@@ -6,3 +6,42 @@ $(window).scroll(function(){
 	});
 });
 
+$(function(){
+    $('.sticky').stickyMenu();
+});
+
+
+(function( $ ){
+    $.fn.stickyMenu = function( options ) {
+		
+        var $this = this,
+        defaults = {
+            'left' : 0,
+            'top' : 0,
+            'menu_offset_top' : $this.offset().top
+        },
+        settings = $.extend({}, defaults, options);
+        $(window).on('scroll.stickyMenu', function(){
+            if ($(window).scrollTop() > settings.menu_offset_top)
+            { 
+                $this.css({ 
+                    'position': 'fixed',
+                    'top':settings.left,
+                    'left':settings.top,
+                    'zIndex':9999
+                });
+                $('body').css({'padding-top': $this.innerHeight()})
+            }
+            else
+            {
+                $this.css({ 
+                    'position': 'relative',
+                }); 
+                $('body').css({'padding-top': 0})
+            }
+        });
+        return $this;
+    };
+})(jQuery);
+
+
